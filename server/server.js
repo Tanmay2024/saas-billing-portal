bbb7d8b007961729783641ef9821c6e4d01d229e
Tanmay2024 <tanmayguruvugari2007@gmail.com>
2026-06-05 20:39:08 +0530

    Initial SaaS Billing Portal setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+const app = express();
+const userRoutes =
+require("./routes/userRoutes");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("SaaS Billing Portal API Running");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
